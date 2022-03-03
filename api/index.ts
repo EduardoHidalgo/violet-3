12609282d69ce3b2a205908a05181c82ee1a0756
Eduardo Hidalgo <ingeduardohidalgo@hotmail.com>
2022-03-02 22:38:01 -0500

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,14 +1,19 @@
 import { ServerApp } from "@/server/app";
 
-/* Self-invoking function to execute and start the API server. */
+/* Self-invoking function to execute and start the API server. Here is the first
+code file to be executed when starting the server. The "ServerApp" class is in 
+charge of applying all the processes and configurations necessary to start the 
+Express.js server. */
 (() => {
   try {
-    console.log("[SERVER] Starting server");
+    console.debug("[SERVER] Starting server");
 
     const server = new ServerApp();
 
     server.init();
-  } catch (e) {
-    console.error(e);
+  } catch (error) {
+    console.error(error);
+    // If server fails to initialize, force aborting Node.js process.
+    process.exit(1);
   }
 })();
